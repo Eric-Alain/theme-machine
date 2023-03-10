d71d65d8826132f3e98f0e343263930f66ff015a
Eric-Alain <ericalain1@hotmail.com>
2023-03-09 19:34:56 -0500

    Dark theme styles to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,13 @@ import * as React from "react"
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto py-3 px-2">
+    <footer className="container mx-auto py-3 px-2 dark:text-tertiary-100">
       © {new Date().getFullYear()} &middot; Built by
       {` `}
-      <a href="https://ericalain.ca" className="hover:text-secondary-900">
+      <a
+        href="https://ericalain.ca"
+        className="text-primary-900 hover:text-secondary-900 dark:text-secondary-400 dark:hover:text-secondary-900"
+      >
         Eric Alain
       </a>
     </footer>
